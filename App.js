3f393950dcf89bb5729052f0782b50fa458e85b3
Gideon Crawley <gscrawley@gmail.com>
2021-07-10 23:44:17 -0700

    tutorial says something should be rendering but seeing blank screen so going to try a text test

---
+++ App.js
@@ -0,0 +1,107 @@
+import { View, TouchableWithoutFeedback, Text } from "react-native";
+import { GLView } from 'expo-gl';
+import { Renderer, TextureLoader } from 'expo-three';
+import * as React from 'react';
+import {
+  AmbientLight,
+  SphereGeometry,
+  Fog,
+  GridHelper,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  PointLight,
+  Scene,
+  SpotLight,
+} from 'three';
+
+export default function App() {
+  let timeout;
+
+  React.useEffect(() => {
+    // Clear the animation loop when the component unmounts
+    return () => clearTimeout(timeout);
+  }, []);
+
+  class SphereMesh extends Mesh {
+    constructor() {
+      super(
+        new SphereGeometry(0, 50, 20, 0, Math.PI * 2, 0, Math.PI * 2),
+        new MeshStandardMaterial({
+          color: 0xff0000,
+        })
+      );
+    }
+  }
+  
+  const sphere = new SphereMesh();
+  const camera = new PerspectiveCamera(100, 0.4, 0.01, 1000);
+
+  let cameraInitialPositionX = 0;
+  let cameraInitialPositionY = 2;
+  let cameraInitialPositionZ = 5;
+
+  <GLView
+  style={{ flex: 1 }}
+  onContextCreate={async (gl) => {
+    // GL Parameter disruption
+    const { drawingBufferWidth: width, drawingBufferHeight: height } = gl;
+
+    // Renderer declaration and set properties
+    const renderer = new Renderer({ gl });
+    renderer.setSize(width, height);
+    renderer.setClearColor("#fff");
+
+    // Scene declaration, add a fog, and a grid helper to see axes dimensions
+    const scene = new Scene();
+    scene.fog = new Fog("#3A96C4", 1, 10000);
+    scene.add(new GridHelper(10, 10));
+
+    // Add all necessary lights
+    const ambientLight = new AmbientLight(0x101010);
+    scene.add(ambientLight);
+
+    const pointLight = new PointLight(0xffffff, 2, 1000, 1);
+    pointLight.position.set(0, 200, 200);
+    scene.add(pointLight);
+
+    const spotLight = new SpotLight(0xffffff, 0.5);
+    spotLight.position.set(0, 500, 100);
+    spotLight.lookAt(scene.position);
+    scene.add(spotLight);
+
+    // Add sphere object instance to our scene
+    scene.add(sphere);
+
+    // Set camera position and look to sphere
+    camera.position.set(
+      cameraInitialPositionX,
+      cameraInitialPositionY,
+      cameraInitialPositionZ
+    );
+
+    camera.lookAt(sphere.position);
+
+    // Render function
+    const render = () => {
+      requestAnimationFrame(render);
+      renderer.render(scene, camera);
+      gl.endFrameEXP();
+    };
+    render();
+  }}
+/>
+
+  return <GLView style={{ flex: 1 }} onContextCreate={async (gl) => {}}/>;
+}
+
+// class IconMesh extends Mesh {
+//   constructor() {
+//     super(
+//       new BoxBufferGeometry(1.0, 1.0, 1.0),
+//       new MeshStandardMaterial({
+//         map: new TextureLoader().load(require('./icon.jpg')),
+//         // color: 0xff0000
+//       })
+//     );
+//   }
